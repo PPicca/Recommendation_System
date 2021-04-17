591b35018f5064d5bbb4e7f92470244132be94e9
PPicca <46577336+PPicca@users.noreply.github.com>
2021-04-17 15:19:06 +0100

    K-means(function)
    simplified function for all cluster size

--- main.py
+++ main.py
@@ -6,59 +6,25 @@ import em
 
 X = np.loadtxt("toy_data.txt")
 
-
-K_1 = 1
-seed_value = 5
-values = []
-for i in range(0,seed_value):
-    M, P = common.init(X, K_1, seed = i)
+def minimum_cost(k):
+    seed_value = 5
+    values = []
+    for i in range(0, seed_value):
+        M, P = common.init(X, k, seed=i)
+        mixture, post, cost = kmeans.run(X, M, P)
+        values.append(cost)
+    min_cost = min(values)
+    min_seed = values.index(min_cost)
+    M, P = common.init(X, k, seed=min_seed)
     mixture, post, cost = kmeans.run(X, M, P)
-    values.append(cost)
-min_cost = min(values)
-min_seed = values.index(min_cost)
-M, P = common.init(X, K_1, seed = min_seed)
-mixture, post, cost = kmeans.run(X, M, P)
-print("The minimum cost of K =", K_1, "is", cost)
-common.plot(X, mixture, post, title = 'K = 1')
+    print("The minimum cost of K =", k, "is", cost)
+    common.plot(X, mixture, post, title=f'K = {k}')
 
-K_2 = 2
-seed_value = 5
-values = []
-for i in range(0,seed_value):
-    M, P = common.init(X, K_2, seed = i)
-    mixture, post, cost = kmeans.run(X, M, P)
-    values.append(cost)
-min_cost = min(values)
-min_seed = values.index(min_cost)
-M, P = common.init(X, K_2, seed = min_seed)
-mixture, post, cost = kmeans.run(X, M, P)
-print("The minimum cost of K =", K_2, "is", cost)
-common.plot(X, mixture, post, title = 'K = 2')
-
-K_3 = 3
-seed_value = 5
-values = []
-for i in range(0,seed_value):
-    M, P = common.init(X, K_3, seed = i)
-    mixture, post, cost = kmeans.run(X, M, P)
-    values.append(cost)
-min_cost = min(values)
-min_seed = values.index(min_cost)
-M, P = common.init(X, K_3, seed = min_seed)
-mixture, post, cost = kmeans.run(X, M, P)
-print("The minimum cost of K =", K_3, "is", cost)
-common.plot(X, mixture, post, title = 'K = 3')
-
-K_4 = 4
-seed_value = 5
-values = []
-for i in range(0,seed_value):
-    M, P = common.init(X, K_4, seed = i)
-    mixture, post, cost = kmeans.run(X, M, P)
-    values.append(cost)
-min_cost = min(values)
-min_seed = values.index(min_cost)
-M, P = common.init(X, K_4, seed = min_seed)
-mixture, post, cost = kmeans.run(X, M, P)
-print("The minimum cost of K =", K_4, "is", cost)
-common.plot(X, mixture, post, title = 'K = 4')
+# TODO ONE Cluster (K=1)
+minimum_cost(1)
+# TODO TWO Clusters (K=2)
+minimum_cost(2)
+# TODO THREE Clusters (K=3)
+minimum_cost(3)
+# TODO FOUR Clusters (K=4)
+minimum_cost(4)
