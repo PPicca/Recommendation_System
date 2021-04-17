c31196f4fe8cb454f26663465802ef78f10611d5
PPicca <46577336+PPicca@users.noreply.github.com>
2021-04-17 13:41:49 +0100

    K -Means seed values
    Trying the optimal seed value

--- main.py
+++ main.py
@@ -5,26 +5,60 @@ import naive_em
 import em
 
 X = np.loadtxt("toy_data.txt")
+
+
 K_1 = 1
-M, P = common.init(X, K_1, seed = 0)
+seed_value = 5
+values = []
+for i in range(0,seed_value):
+    M, P = common.init(X, K_1, seed = i)
+    mixture, post, cost = kmeans.run(X, M, P)
+    values.append(cost)
+min_cost = min(values)
+min_seed = values.index(min_cost)
+M, P = common.init(X, K_1, seed = min_seed)
 mixture, post, cost = kmeans.run(X, M, P)
-print("The cost of K =", K_1, "is", cost)
+print("The minimum cost of K =", K_1, "is", cost)
 common.plot(X, mixture, post, title = 'K = 1')
 
 K_2 = 2
-M, P = common.init(X, K_2, seed = 0)
+seed_value = 5
+values = []
+for i in range(0,seed_value):
+    M, P = common.init(X, K_2, seed = i)
+    mixture, post, cost = kmeans.run(X, M, P)
+    values.append(cost)
+min_cost = min(values)
+min_seed = values.index(min_cost)
+M, P = common.init(X, K_2, seed = min_seed)
 mixture, post, cost = kmeans.run(X, M, P)
-print("The cost of K =", K_2, "is", cost)
+print("The minimum cost of K =", K_2, "is", cost)
 common.plot(X, mixture, post, title = 'K = 2')
 
 K_3 = 3
-M, P = common.init(X, K_3, seed = 0)
+seed_value = 5
+values = []
+for i in range(0,seed_value):
+    M, P = common.init(X, K_3, seed = i)
+    mixture, post, cost = kmeans.run(X, M, P)
+    values.append(cost)
+min_cost = min(values)
+min_seed = values.index(min_cost)
+M, P = common.init(X, K_3, seed = min_seed)
 mixture, post, cost = kmeans.run(X, M, P)
-print("The cost of K =", K_3, "is", cost)
+print("The minimum cost of K =", K_3, "is", cost)
 common.plot(X, mixture, post, title = 'K = 3')
 
 K_4 = 4
-M, P = common.init(X, K_4, seed = 0)
+seed_value = 5
+values = []
+for i in range(0,seed_value):
+    M, P = common.init(X, K_4, seed = i)
+    mixture, post, cost = kmeans.run(X, M, P)
+    values.append(cost)
+min_cost = min(values)
+min_seed = values.index(min_cost)
+M, P = common.init(X, K_4, seed = min_seed)
 mixture, post, cost = kmeans.run(X, M, P)
-print("The cost of K =", K_4, "is", cost)
+print("The minimum cost of K =", K_4, "is", cost)
 common.plot(X, mixture, post, title = 'K = 4')
