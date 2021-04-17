e9a0f510775faefb31eab769ee45c2ffe6ef9cc3
PPicca <46577336+PPicca@users.noreply.github.com>
2021-04-17 12:44:19 +0100

    K-means
    for different cluster sizes

--- main.py
+++ main.py
@@ -6,7 +6,25 @@ import em
 
 X = np.loadtxt("toy_data.txt")
 K_1 = 1
-M, P = common.init(X, K_1, seed = 4)
+M, P = common.init(X, K_1, seed = 0)
 mixture, post, cost = kmeans.run(X, M, P)
 print("The cost of K =", K_1, "is", cost)
 common.plot(X, mixture, post, title = 'K = 1')
+
+K_2 = 2
+M, P = common.init(X, K_2, seed = 0)
+mixture, post, cost = kmeans.run(X, M, P)
+print("The cost of K =", K_2, "is", cost)
+common.plot(X, mixture, post, title = 'K = 2')
+
+K_3 = 3
+M, P = common.init(X, K_3, seed = 0)
+mixture, post, cost = kmeans.run(X, M, P)
+print("The cost of K =", K_3, "is", cost)
+common.plot(X, mixture, post, title = 'K = 3')
+
+K_4 = 4
+M, P = common.init(X, K_4, seed = 0)
+mixture, post, cost = kmeans.run(X, M, P)
+print("The cost of K =", K_4, "is", cost)
+common.plot(X, mixture, post, title = 'K = 4')
