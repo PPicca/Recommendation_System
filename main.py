b9089979121a7e74fae7d6dad6307f1f31198e79
PPicca <46577336+PPicca@users.noreply.github.com>
2021-04-19 19:20:11 +0100

    Maximum_Likelihood function
    function that returns the maximize likelihood for K clusters.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import em
 
 X = np.loadtxt("toy_data.txt")
 
-def minimum_cost(k):
+def kmeans_minimum_cost(k):
     seed_value = 5
     values = []
     for i in range(0, seed_value):
@@ -21,11 +21,41 @@ def minimum_cost(k):
     print("The minimum cost of K =", k, "is", cost)
     common.plot(X, mixture, post, title=f'K = {k}')
 
-# TODO ONE Cluster (K=1)
-minimum_cost(1)
-# TODO TWO Clusters (K=2)
-minimum_cost(2)
-# TODO THREE Clusters (K=3)
-minimum_cost(3)
-# TODO FOUR Clusters (K=4)
-minimum_cost(4)
+# ONE Cluster (K=1)
+kmeans_minimum_cost(1)
+
+# TWO Clusters (K=2)
+kmeans_minimum_cost(2)
+
+# THREE Clusters (K=3)
+kmeans_minimum_cost(3)
+
+# FOUR Clusters (K=4)
+kmeans_minimum_cost(4)
+
+def maximum_likelihood(k):
+    seed_value = 5
+    values = []
+    for i in range(0, seed_value):
+        M, P = common.init(X, k, seed=i)
+        mixture, post, ll = naive_em.run(X, M, P)
+        values.append(ll)
+    max_ll = max(values)
+    max_seed = values.index(max_ll)
+
+    M, P = common.init(X, k, seed=max_seed)
+    mixture, post, ll = naive_em.run(X, M, P)
+    print("The highest likelihood of K =", k, "is", ll)
+    common.plot(X, mixture, post, title=f'K = {k}')
+
+# ONE Cluster (K=1)
+maximum_likelihood(1)
+
+# TWO Clusters (K=2)
+maximum_likelihood(2)
+
+# THREE Clusters (K=3)
+maximum_likelihood(3)
+
+# FOUR Clusters (K=4)
+maximum_likelihood(4)
