f8183118f245fb3af18ef4a57095e8e49d54f5b1
PPicca <46577336+PPicca@users.noreply.github.com>
2021-04-27 14:12:22 +0100

    test

--- test.py
+++ test.py
@@ -9,7 +9,22 @@ K = 4
 n, d = X.shape
 seed = 0
 
-# TODO: Your code here
-M, P = common.init(X, K, seed)
+seed_value = 5
+values = []
+for i in range(0, seed_value):
+    M_1, P_1 = common.init(X, K, seed=i)
+    mix, pT, l = em.run(X, M_1, P_1)
+    values.append(l)
+max_ll = max(values)
+max_seed = values.index(max_ll)
+
+M, P = common.init(X, K, seed=max_seed)
 mixture, post, ll = em.run(X, M, P)
-print(ll)
+
+X_pred = em.fill_matrix(X, mixture)
+rmse = common.rmse(X_gold, X_pred)
+
+if rmse == 0.3152301205749675:
+    print("PASS")
+else:
+    print("FAIL")
